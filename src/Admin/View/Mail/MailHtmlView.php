13a9ddcdc9ccca22821693bc19416e9840894519
Simon Asika <asika32764@gmail.com>
2016-05-19 09:08:22 +0800

    Mail View #5

---
+++ src/Admin/View/Mail/MailHtmlView.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Lyrasoft\Luna\Admin\View\Mail;
+
+use Phoenix\View\AbstractPhoenixHtmView;
+
+/**
+ * The MailHtmlView class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class MailHtmlView extends AbstractPhoenixHtmView
+{
+
+}
