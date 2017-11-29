2b70b96e1482aed3e928d8ef1ce495293b182ec0
Simon Asika <asika32764@gmail.com>
2017-11-29 14:47:34 +0800

    Use new style buttons

--- src/Admin/Templates/language/toolbar.blade.php
+++ src/Admin/Templates/language/toolbar.blade.php
@@ -13,24 +13,43 @@
  */
 ?>
 
-<button type="button" class="btn btn-success btn-sm" onclick="Phoenix.post();">
-    <span class="glyphicon glyphicon-floppy-disk fa fa-save"></span>
-    @translate('phoenix.toolbar.save')
-</button>
+<div class="btn-group">
+    <button type="button" class="btn btn-success btn-sm phoenix-btn-save"
+        onclick="Phoenix.post();">
+        <span class="fa fa-save"></span>
+        @translate('phoenix.toolbar.save')
+    </button>
+    <button type="button" class="btn btn-success btn-sm dropdown-toggle dropdown-toggle-split"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="caret"></span>
+        <span class="sr-only">Toggle Dropdown</span>
+    </button>
 
-<button type="button" class="btn btn-default btn-primary btn-sm" onclick="Phoenix.post(null, {task: 'save2close'});">
-    <span class="glyphicon glyphicon-ok fa fa-check"></span>
+    <ul class="dropdown-menu">
+        <li>
+            <a class="dropdown-item phoenix-btn-save2copy"
+                href="javascript://"
+                onclick="Phoenix.post(null, {task: 'save2copy'});">
+                <span class="fa fa-copy text-info"></span>
+                @translate('phoenix.toolbar.save2copy')
+            </a>
+        </li>
+
+        <li>
+            <a class="dropdown-item phoenix-btn-save2new"
+                href="javascript://"
+                onclick="Phoenix.post(null, {task: 'save2new'});">
+                <span class="fa fa-plus text-primary"></span>
+                @translate('phoenix.toolbar.save2new')
+            </a>
+        </li>
+    </ul>
+</div>
+
+<button type="button" class="btn btn-primary btn-outline-primary btn-sm phoenix-btn-save2close"
+    onclick="Phoenix.post(null, {task: 'save2close'});">
+    <span class="fa fa-check"></span>
     @translate('phoenix.toolbar.save2close')
-</button>
-
-<button type="button" class="btn btn-default btn-info btn-sm" onclick="Phoenix.post(null, {task: 'save2copy'});">
-    <span class="glyphicon glyphicon-duplicate fa fa-copy"></span>
-    @translate('phoenix.toolbar.save2copy')
-</button>
-
-<button type="button" class="btn btn-default btn-info btn-sm" onclick="Phoenix.post(null, {task: 'save2new'});">
-    <span class="glyphicon glyphicon-plus fa fa-plus"></span>
-    @translate('phoenix.toolbar.save2new')
 </button>
 
 <a role="button" class="btn btn-default btn-outline-secondary btn-sm" href="{{ $router->route('languages') }}">
