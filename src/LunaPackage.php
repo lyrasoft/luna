fb4997ca359a7bde48afaa73f5369b23a6d845f0
Simon Asika <asika32764@gmail.com>
2018-11-21 20:42:29 +0800

    Update LunaPackage.php

--- src/LunaPackage.php
+++ src/LunaPackage.php
@@ -1,134 +1,90 @@
 <?php
 /**
- * Part of phoenix project.
+ * Part of earth project.
  *
  * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
  * @license    GNU General Public License version 2 or later.
  */
 
-namespace Lyrasoft\Luna;
+namespace Lyrasoft\Luna\Provider;
 
-use Lyrasoft\Luna\Helper\LunaHelper;
-use Lyrasoft\Luna\Module\ModuleHelper;
-use Lyrasoft\Luna\PageBuilder\AddonHelper;
-use Windwalker\Core\Language\Translator;
-use Windwalker\Core\Package\AbstractPackage;
-
-define('LUNA_ROOT', dirname(__DIR__));
-define('LUNA_SOURCE', LUNA_ROOT . '/src');
-define('LUNA_SOURCE_ADMIN', LUNA_SOURCE . '/Admin');
+use Lyrasoft\Luna\Config\ConfigService;
+use Lyrasoft\Luna\LunaPackage;
+use Lyrasoft\Luna\PageBuilder\PageBuilder;
+use Windwalker\Core\Renderer\RendererManager;
+use Windwalker\DI\Container;
+use Windwalker\DI\ServiceProviderInterface;
+use Windwalker\Utilities\Queue\PriorityQueue;
 
 /**
- * The LunaPackage class.
+ * The LunaProvider class.
  *
  * @since  1.0
  */
-class LunaPackage extends AbstractPackage
+class LunaProvider implements ServiceProviderInterface
 {
     /**
-     * WarderPackage constructor.
+     * Property luna.
+     *
+     * @var  LunaPackage
      */
-    public function __construct()
+    protected $luna;
+
+    /**
+     * LunaProvider constructor.
+     *
+     * @param  LunaPackage $luna
+     */
+    public function __construct(LunaPackage $luna)
     {
-        LunaHelper::setPackage($this);
+        $this->luna = $luna;
     }
 
     /**
-     * initialise
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container $container The DI container.
      *
      * @return  void
-     * @throws \ReflectionException
-     * @throws \Windwalker\DI\Exception\DependencyResolutionException
      */
-    public function boot()
+    public function register(Container $container)
     {
-        parent::boot();
+        $container->getParent()->prepareSharedObject(ConfigService::class);
 
-        Translator::loadAll($this);
+        $this->registerClassAlias();
 
-        ModuleHelper::addPath(__NAMESPACE__ . '\Module', $this->getDir() . '/Module');
-        AddonHelper::addPath(__NAMESPACE__ . '\PageBuilder', $this->getDir() . '/PageBuilder');
-    }
-
-    /**
-     * isFrontend
-     *
-     * @param   string $name
-     *
-     * @return  boolean
-     */
-    public function isFrontend($name = null)
-    {
-        $package = $this->getCurrentPackage();
-
-        if (!$package) {
-            return false;
+        if ($container->get('application')->isConsole()) {
+            return;
         }
 
-        $name = $name ?: $package->getName();
+        $container->getParent()->extend(
+            RendererManager::class,
+            function (RendererManager $manager, Container $container) {
+                $manager->addGlobalPath($this->luna->getDir() . '/Resources/templates', PriorityQueue::LOW - 25);
 
-        return in_array($name, (array) $this->get('frontend.package', 'front'), true);
+                return $manager;
+            }
+        );
+
+        $container->prepareSharedObject(PageBuilder::class);
     }
 
     /**
-     * isFrontend
+     * registerClassAlias
      *
-     * @param   string $name
+     * @return  void
      *
-     * @return  boolean
+     * @since  __DEPLOY_VERSION__
      */
-    public function isAdmin($name = null)
+    protected function registerClassAlias()
     {
-        $package = $this->getCurrentPackage();
+        static $registered = false;
 
-        if (!$package) {
-            return false;
+        if (!$registered) {
+            class_alias(\Lyrasoft\Luna\Repository\CategoriesRepository::class, \Lyrasoft\Luna\Repository\CategoriesModel::class);
+            class_alias(\Lyrasoft\Luna\Repository\ArticlesRepository::class, \Lyrasoft\Luna\Repository\ArticlesModel::class);
         }
 
-        $name = $name ?: $package->getName();
-
-        return in_array($name, (array) $this->get('admin.package', 'admin'), true);
-    }
-
-    /**
-     * isEnabled
-     *
-     * @param   string $name
-     *
-     * @return  boolean
-     */
-    public function isEnabled($name = null)
-    {
-        return $this->isFrontend($name) || $this->isAdmin($name);
-    }
-
-    /**
-     * getCurrentPackage
-     *
-     * @return  AbstractPackage
-     */
-    public function getCurrentPackage()
-    {
-        if (!$this->container->exists('current.package')) {
-            return null;
-        }
-
-        return $this->container->get('current.package');
-    }
-
-    /**
-     * getLangPrefix
-     *
-     * @return  string
-     */
-    public function getLangPrefix()
-    {
-        if ($this->isAdmin()) {
-            $langPrefix = $this->get('admin.language.prefix', 'luna.');
-        } else {
-            $langPrefix = $this->get('frontend.language.prefix', 'luna.');
-        }
-
-        return $langPrefix;
+        $registered = true;
     }
 }
