926ae9099a5d0e701aa4ce7b42f698bcfc28e039
Simon Asika <asika32764@gmail.com>
2019-01-22 00:40:24 +0800

    Update LunaProvider.php

--- src/Provider/LunaProvider.php
+++ src/Provider/LunaProvider.php
@@ -81,8 +81,8 @@ class LunaProvider implements ServiceProviderInterface
         static $registered = false;
 
         if (!$registered) {
-            class_alias(\Lyrasoft\Luna\Repository\CategoriesRepository::class, \Lyrasoft\Luna\Repository\CategoriesModel::class);
-            class_alias(\Lyrasoft\Luna\Repository\ArticlesRepository::class, \Lyrasoft\Luna\Repository\ArticlesModel::class);
+            class_alias(\Lyrasoft\Luna\Repository\CategoriesRepository::class, \Lyrasoft\Luna\Model\CategoriesModel::class);
+            class_alias(\Lyrasoft\Luna\Repository\ArticlesRepository::class, \Lyrasoft\Luna\Model\ArticlesModel::class);
         }
 
         $registered = true;
