34ae017cddaa611ec0f191fbe1e01ac5d047967e
Simon Asika <asika32764@gmail.com>
2023-06-05 15:02:39 +0800

    misc

--- assets/src/vue/services/page-builder/directives/tooltip.js
+++ assets/src/vue/services/page-builder/directives/tooltip.js
@@ -9,10 +9,10 @@ export default class BsTooltip {
   static install(app) {
     app.directive('tooltip', {
       mounted(el, { value }) {
-        const inc = u.$ui.bootstrap.tooltip(el, value);
+        const inc = u.$ui.bootstrap.tooltip(el, value || {});
       },
       updated(el, { value }) {
-        const inc = u.$ui.bootstrap.tooltip(el, value);
+        const inc = u.$ui.bootstrap.tooltip(el, value || {});
 
         inc.update();
       },
